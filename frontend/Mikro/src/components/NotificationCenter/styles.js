19ccec469224845023648f50c13401e9e3a983e6
Devin Markley <devin.markley86@gmail.com>
2026-05-12 14:03:38 -0500

    remove old reference Mikro

--- frontend/Mikro/src/components/NotificationCenter/styles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const NotificationCenterWrapper = styled.div`
-  position: absolute;
-  z-index: 1000;
-  top: 15%;
-  left: 15px; // = Grid containerPadding => 10px + 5px of desired left margin
-`;
--- frontend/Mikro/src/components/constants.js
+++
@@ -1,6 +0,0 @@
-//PROD
-export const API_URL = "/api/";
-export const SSO_URL = "https://my.kaart.com/api/";
-//DEV
-// export const API_URL = "http://dev.localhost:5004/api/";
-// export const SSO_URL = "http://dev.localhost:5001/api/";
